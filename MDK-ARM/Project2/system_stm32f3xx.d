07cbefb395839f9d4440c8b168ed681991a6ca37
arashdann <a.rashdan98@aucegypt.edu>
2020-11-27 18:40:45 +0200

    STM32 Code
    microcontroller code that reads temperature and sends it through UART2

---
+++ MDK-ARM/Project2/system_stm32f3xx.d
@@ -0,0 +1,30 @@
+project2\system_stm32f3xx.o: ../Core/Src/system_stm32f3xx.c
+project2\system_stm32f3xx.o: ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h
+project2\system_stm32f3xx.o: ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303x8.h
+project2\system_stm32f3xx.o: ../Drivers/CMSIS/Include/core_cm4.h
+project2\system_stm32f3xx.o: D:\Uni\Fall 2020\CSCE 4302\MDK\ARM\ARMCC\Bin\..\include\stdint.h
+project2\system_stm32f3xx.o: ../Drivers/CMSIS/Include/cmsis_version.h
+project2\system_stm32f3xx.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+project2\system_stm32f3xx.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+project2\system_stm32f3xx.o: ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h
+project2\system_stm32f3xx.o: ../Core/Inc/stm32f3xx_hal_conf.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h
+project2\system_stm32f3xx.o: ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h
+project2\system_stm32f3xx.o: D:\Uni\Fall 2020\CSCE 4302\MDK\ARM\ARMCC\Bin\..\include\stddef.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_exti.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart.h
+project2\system_stm32f3xx.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart_ex.h
